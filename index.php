608bd51621c7211c6a1b08198bc95813e5212370
nicola00 <nicolajobs00@gmail.com>
2023-03-30 15:35:52 +0200

    FEAT constract

--- index.php
+++ index.php
@@ -2,28 +2,36 @@
 
 include_once __DIR__. './models/movies.php';
 
-$film1 = new Movies ();
-$film2 = new Movies ();
+// $film1 = new Movies ();
+// $film2 = new Movies ();
 
-//titolo film
-$film1->title = "Avengers"; 
-$film2->title = "Batman Begins";
+// //titolo film
+// $film1->title = "Avengers"; 
+// $film2->title = "Batman Begins";
 
-//regista dei film
-$film1->regista = "Anthony Russo";
-$film2->regista = "Christopher Nolan";
+// //regista dei film
+// $film1->regista = "Anthony Russo";
+// $film2->regista = "Christopher Nolan";
 
-//attori del film
-$film1->attori = "Robert Downey Jr., Scarlett Johansson, Chris Evans, Mark Ruffalo, Chris Hemsworth, Jeremy Renner ";
-$film2->attori = "Christian Bale, Cillian Murphy, Katie Holmes, Michael Caine";
+// //attori del film
+// $film1->attori = "Robert Downey Jr., Scarlett Johansson, Chris Evans, Mark Ruffalo, Chris Hemsworth, Jeremy Renner ";
+// $film2->attori = "Christian Bale, Cillian Murphy, Katie Holmes, Michael Caine";
 
-//attori del film
-$film1->valutazione = 4.5;
-$film2->valutazione = 4.8;
+// //attori del film
+// $film1->valutazione = 4.5;
+// $film2->valutazione = 4.8;
+
+//nuvo fil con il costruttore
+$film3 = new Movies("Everything Everywhere All at Once",
+ " Daniel Kwan, Daniel Scheinert", 
+ "Michelle Yeoh, Ke Huy Quan, Stephanie Hsu, James Hong",
+ 4.7);
 
 
-var_dump($film1);
-var_dump($film2);
+// var_dump($film1);
+// var_dump($film2);
+var_dump($film3);
+
 
 ?>
 <!DOCTYPE html>
--- models/movies.php
+++ models/movies.php
@@ -8,6 +8,11 @@ class Movies {
     public $attori;
     public $valutazione;
 
-    
+    function __construct($_title, $_regista, $_attori, $_valutazione) {
+        $this->title = $_title;
+        $this->regista = $_regista;
+        $this->attori= $_attori;
+        $this->valutazione = $_valutazione;
+    }
     
 }
